667879f413b8a8ebc0447cc73b0629863490936e
Vinayaksahu <vinayaksahu3@gmail.com>
2023-06-11 23:56:44 +0530

    check input value integer or not

--- src/UserInput.java
+++ src/UserInput.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
 
-        System.out.println("Enter 3 numbers: ");
-
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 3 numbers: ");
         int firstNum = input.nextInt();
         float secondNum = input.nextFloat();
         byte thirdNum = input.nextByte();
@@ -12,6 +12,11 @@ public class UserInput {
         double sum = firstNum + secondNum + thirdNum;
 
         System.out.println("Sum of all inputs: " + sum);
+
+        //check input value integer or not
+        System.out.println("Checking input key is integer or not :" + input.hasNextInt());
+        /*boolean isInteger = input.hasNextInt();
+        System.out.println("Checking input key is integer or not :" + isInteger);*/
     }
 }
 
