4e7a126613123ccbb12614d2ca56cc95c8583247
Vinayaksahu <vinayaksahu3@gmail.com>
2023-06-13 19:25:35 +0530

    string practice

---
+++ src/String13th.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class String13th {
+    public static void main(String[] args) {
+
+        //String name1 = "Vinayak";
+        String name1;
+        name1 = "Vinayak";
+        System.out.println(name1);
+
+        String name2 = new String("Sahu");
+        System.out.println(name2);
+
+        Scanner input = new Scanner(System.in);
+        //String name3 = input.next();
+        String name3 = input.nextLine();
+        System.out.println(name3);
+    }
+}
