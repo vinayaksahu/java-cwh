bd798789cf5b6a781232a6410f8a4be7c6c4777c
Vinayaksahu <vinayaksahu3@gmail.com>
2024-02-19 12:37:07 +0530

    Array reverse print

---
+++ src/ReverseArray.java
@@ -0,0 +1,18 @@
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6};
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
