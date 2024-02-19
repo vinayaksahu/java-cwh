442cd378c4ff4d737bb41aead4286b99d8930c56
Vinayaksahu <vinayaksahu3@gmail.com>
2024-02-19 12:09:09 +0530

    java practice started

--- src/UserInput.java
+++ src/UserInput.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter 3 numbers: ");
+            int firstNum = input.nextInt();
+            float secondNum = input.nextFloat();
+            byte thirdNum = input.nextByte();
 
-        System.out.println("Enter 3 numbers: ");
-        int firstNum = input.nextInt();
-        float secondNum = input.nextFloat();
-        byte thirdNum = input.nextByte();
+            double sum = firstNum + secondNum + thirdNum;
 
-        double sum = firstNum + secondNum + thirdNum;
+            System.out.println("Sum of all inputs: " + sum);
 
-        System.out.println("Sum of all inputs: " + sum);
-
-        //check input value integer or not
-        System.out.println("Checking input key is integer or not :" + input.hasNextInt());
-        /* boolean isInteger = input.hasNextInt();
-        System.out.println("Checking input key is integer or not :" + isInteger); */
+            //check input value integer or not
+            System.out.println("Checking input key is integer or not :" + input.hasNextInt());
+            /* boolean isInteger = input.hasNextInt();
+            System.out.println("Checking input key is integer or not :" + isInteger); */
+        }
     }
 }
 
