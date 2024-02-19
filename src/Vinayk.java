8a8e3f52dbb48af5894cfd7eab413823eceb9a55
Vinayaksahu <vinayaksahu3@gmail.com>
2024-02-19 12:21:03 +0530

    java input

--- src/Vinayk.java
+++ src/Vinayk.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Vinayk {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.print("Enter any number: ");
+
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+
+        System.out.println("you have entered " + num );
     }
 }
